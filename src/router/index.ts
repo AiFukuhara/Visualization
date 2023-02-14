c8a89499a91910cf6cb8f7bbefac8694373cc2f3
rizo <h2434530141@icloud.com>
2023-02-14 19:50:38 +0800

    添加路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import SwustMap from '../components/SwustMap.vue';
-
+import HomeView from '../views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: SwustMap
-    }
     // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    //   path: '/',
+    //   name: 'home',
+    //   component: SwustMap
+    // },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path:'/homepage',
+      name:'homepage',
+      component:HomeView  
+    }
   ]
 })
 
